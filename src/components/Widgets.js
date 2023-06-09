8233e0d49f5d840b330e925c245950d5701be927
Pablo Andres Ramirez Lopez <pabloa1311@gmail.com>
2023-06-09 15:50:15 -0500

    Widgets section created. MVP of the project

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import "../styles/Widgets.css"
 import InfoIcon from '@mui/icons-material/Info';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 function Widgets() {
+    const newsArticle = (heading, subtitle) => {
+        return (<div className='widgets__article'>
+            <div className='widgets__articleLeft'>
+                <FiberManualRecordIcon />
+            </div>
+
+            <div className='widgets__articleRight'>
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>)
+        
+    }
+
   return (
     <div className='widgets'>
         <div className='widgets__header'>
             <h2>LinkedIn News</h2>
             <InfoIcon />
         </div>
+        {newsArticle("Pabloo quiere ser NDV", "Cuandooo se arriesgará???")}
+        {newsArticle("Loquito por ti", "Loco loco")}
+        {newsArticle("Loquito por ti", "Por ti por ti")}
+        {newsArticle("Noticia 3", "jsjdjs djjd jdjdd")}
+        {newsArticle("Noticia 4", "jsjdjs djjd jdjdd")}
+        {newsArticle("Noticia 5", "jsjdjs djjd jdjdd")}
     </div>
   )
 }
